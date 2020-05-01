e4c49211316f4d5779711e6dfa67212930729330
Hajimirzaei <shajimirzaei@gmail.com>
2020-05-01 20:38:33 +0430

    added integration test with development db

---
+++ SampleProject.IntegrationTest/APIWebApplicationFactory.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using SampleProject.WebApi;
+
+namespace SampleProject.IntegrationTest
+{
+    public class APIWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+    }
+}
---
+++ SampleProject.IntegrationTest/ApiTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProject.IntegrationTest
+{
+    [TestFixture]
+    public class ApiTests
+    {
+        private APIWebApplicationFactory _factory;
+        private HttpClient _client;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _factory = new APIWebApplicationFactory();
+            _client = _factory.CreateClient();
+        }
+
+        [Test]
+        public async Task Get_Courses_Should_Be_Ok()
+        {
+            // Arrange
+            var request = "/api/Course/Get";
+
+            // Act
+            var response = await _client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+
+
+        [Test]
+        public async Task Register_Course_Should_Be_Ok()
+        {
+            // Arrange
+            var request = new {
+                Url = "/api/Course/RegisterCourse",
+                Body = new
+                {
+                    StudentId = 1,
+                    SelectedCourseCodes = new List<int>() { 1, 2 }
+                }
+            };
+
+            // Act
+            var response = await _client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
+    }
+}
