0da1d6abcdf5cb2d84c88f6e6478c31c158a31b1
Hajimirzaei <shajimirzaei@gmail.com>
2020-05-10 23:25:52 +0430

    bug fixing at repository unit test and response message

---
+++ SampleProject.WebApi/Mapper/MapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SampleProject.Core.DTOs;
+using SampleProject.Core.Entities;
+
+namespace SampleProject.WebApi.Mapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Course, CourseVM>().ReverseMap();
+        }
+    }
+}
